cad103439c28efa0b1885ae3f2a5df5f38c22b0d
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-12 11:37:02 -0700

    Add files via upload
    Starter Code for our Collatz sequence

---
+++ collztzStarter.py
@@ -0,0 +1,18 @@
+//Define a function that passes in a parameter of an integer
+def collatz(number):
+
+    //If the number is even, print the number divided by 2 then return the number divided by 2
+    if :
+
+    //Else if the number is an odd number, the result will be 3 times the number + 1. Then print the result and return the result
+    elif :
+
+//Try the block of code given no exception is detected. Call the user input function to allow keyboard input and store the user input in the variable 'n'
+try:
+    n = input("Give me a number: ")
+    //As long as the input number is not equal to one, call the collatz function on the integer value of the number passed in
+
+
+// Exception: if an invalid input is given, print the error message
+except ValueError:
+    print('Value Error. Please enter an Integer')
